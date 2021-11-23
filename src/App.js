feb3d31cd860ca51244d061612299d9515777ceb
James Spencer <jameskcs1254@gmail.com>
2021-11-23 19:03:00 +0000

    add basic styling and JSX markup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+      <header>Nature Memory Game</header>
+      <main>
+        <div className="card-container"></div>
+      </main>
+      <footer>Made by James Spencer</footer>
     </div>
   );
 }
