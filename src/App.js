a6a2c9c24a9fc6b61ede517abf59b1a5002444e1
James Spencer <jameskcs1254@gmail.com>
2021-11-23 19:12:59 +0000

    ad Fischer-Yates shuffule

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import './App.css';
 
 const App = () => {
+  const shuffle = (array) => {
+    // Fisher-Yates shuffle algorithm.
+    let currentIndex = array.length,  randomIndex;
+  
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
   return (
     <div className="App">
       <header>Nature Memory Game</header>
