8506a9f651a5dfcc8db31a3a84c04ac8a4918afb
James Spencer <jameskcs1254@gmail.com>
2021-11-23 19:24:16 +0000

    add state variables

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import './App.css';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [memoryArray, setMemoryArray] = useState([
+    'Forest',
+    'Mountain',
+    'Swamp',
+    'Lake',
+    'Ocean',
+    'Mangrove',
+    'Coral Reef',
+    'Hot Springs',
+    'Volcano',
+    'Savanna',
+    'Rain Forest',
+    'River'
+  ])
+  const [score, setScore] = useState(0)
+  const [highScore, setHighScore] = useState(0)
+
   const shuffle = (array) => {
     // Fisher-Yates shuffle algorithm.
     let currentIndex = array.length,  randomIndex;
